ce0ff70769213db4246ed6ca925d7ade458e12ea
Daniele Senko <daniele.senko@unemat.br>
2024-11-15 09:12:01 -0400

    feat: implement Home page layout and styling
    - Added the Home page component with initial structure and content.
    - Applied CSS styling for the Home page, including responsive design.
    - Ensured a consistent look and feel with global styles.
    - Integrated the Home page into the application routing.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,74 @@
-import './style.css'
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import Trash from "../../assets/bxs-trash-alt.svg";
+import api from "../../services/api";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function getUsers() {
+    const usersFromApi = await api.get("/usuarios");
+
+    setUsers(usersFromApi.data);
+    console.log(users);
+  }
+
+  async function createUsers() {
+    await api.post("/usuarios", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value,
+    });
+
+    getUsers();
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`)
+
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
-    <div>
-      
+    <div className="container">
+      <form action="">
+        <h1>Cadastro de Usuários</h1>
+        <input name="nome" placeholder="Nome" type="text" ref={inputName} />
+        <input name="idade" placeholder="Idade" type="number" ref={inputAge} />
+        <input name="email" placeholder="Email" type="email" ref={inputEmail} />
+        <button type="button" onClick={createUsers}>
+          Cadastrar
+        </button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>
+              Nome: <span>{user.name}</span>
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>
+            </p>
+            <p>
+              Email: <span>{user.email}</span>
+            </p>
+          </div>
+          <button onClick={() => deleteUsers(user.id)}>
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
