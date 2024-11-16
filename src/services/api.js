c069b8a5ebbab82783a82b9d459161fe1dfbd633
Daniele Senko <daniele.senko@unemat.br>
2024-11-16 05:14:10 -0400

    refactor: baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://noon-bubble-pudding.glitch.me'
+    baseURL: 'https://api-cadastro-usuarios-53nf.onrender.com/'
 })
 
 export default api;
