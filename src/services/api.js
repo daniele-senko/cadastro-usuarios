cef55b707de2775140683361970ebcf6a077a265
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2025-10-22 07:24:15 +0000

    fix(frontend): correct Axios baseURL for API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'cadastro-usuarios-new.vercel.app/usuarios'
+    baseURL: 'https://cadastro-usuarios-new.vercel.app/usuarios'
 })
 
 export default api;
